1c33c0d6e36e475f1a8aa236795e3a8d2fc17fea
matheus-bruscke <matheus.bruscke@gmail.com>
2026-08-11 21:58:46 -0400

    chore: add drizzle base configs

---
+++ apps/server/src/infra/db/index.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config'
+import { drizzle } from 'drizzle-orm/node-postgres'
+
+if (typeof process.env.DATABASE_URL !== 'string') {
+  throw new Error('Invalid Database URL')
+}
+
+const db = drizzle(process.env.DATABASE_URL)
+
+export { db }
