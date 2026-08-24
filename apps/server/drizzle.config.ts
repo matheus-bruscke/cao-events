d349bb5baa9464e7d1c586bea38c18d2314fd3b7
matheus-bruscke <matheus.bruscke@gmail.com>
2026-08-24 09:58:33 -0400

    chore: update drizzle configuration paths for database and schema

--- apps/server/drizzle.config.ts
+++ apps/server/drizzle.config.ts
@@ -2,8 +2,8 @@ import 'dotenv/config'
 import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
-  out: './drizzle',
-  schema: './src/infra/db/schema.ts',
+  out: './infra/database',
+  schema: './src/infra/database/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
